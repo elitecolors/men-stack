88479846658ce7e8b4219c81d2d38414fcc88e9d
Ahmed <saidaniahmed125@gmail.com>
2020-02-17 16:16:56 +0100

    adding an image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   loading = false;
   form: FormGroup;
+  imagePreview;
 
   private mode =  'create';
   private postId: string;
@@ -26,8 +27,8 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl(null,
                            {validators: [Validators.required, Validators.minLength(3)]} ),
-      content: new FormControl(null,
-      {validators: [Validators.required]} )
+      content: new FormControl(null, {validators: [Validators.required]} ),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if ( paramMap.has('postId')) {
@@ -64,6 +65,18 @@ export class PostCreateComponent implements OnInit {
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
     this.form.reset();
+
+  }
+
+  onImagePicked(event: Event) {
+      const file = (event.target as HTMLInputElement).files[0];
+      this.form.patchValue({image: file});
+      this.form.get('image').updateValueAndValidity();
+     const reader= new FileReader();
+     reader.onload= ()=>{
+        this.imagePreview=reader.result;
+     }
+     reader.readAsDataURL(file);
 
   }
 
